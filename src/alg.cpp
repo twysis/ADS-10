d1c20f13873b312a75eca802e77a4450249f2887
Матвей Головенко <91070065+twysis@users.noreply.github.com>
2022-06-03 14:07:42 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,4 +7,10 @@
 
 std::vector<char> getPerm(const Tree& tree, int n) {
   // напишите реализацию
+  std::string optionsTree = tree[n - 1];
+  std::vector<char> numbers;
+  for (int i = 0; i < optionsTree.length(); i++) {
+    numbers.push_back(optionsTree[i]);
+  }
+  return numbers;
 }
